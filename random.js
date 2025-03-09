a4bce3076abe0ac181b2508a27372527a4e0e27e
Bart Simons Comic <benstoeckl@outlook.com>
2025-03-09 17:40:32 +0100

    Update random.js

--- random.js
+++ random.js
@@ -5,3 +5,7 @@ function randlink() {
     const ra = Math.floor(Math.random() * links.length);
     return links[ra];
 }
+document.getElementById('randomLinkBtn').addEventListener('click', function() {
+    const randomLink = randlink();
+    window.location.href = randomLink; // Redirect to the random link
+});
